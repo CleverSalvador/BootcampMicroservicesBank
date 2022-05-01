88fd237bb5f10ae2d81c8c0d983c09e1111c15ec
CleverSalvador <85046870+CleverSalvador@users.noreply.github.com>
2022-04-30 22:48:18 -0500

    eureka_client

--- client-service/src/main/java/com/clientservice/app/ClientServiceApplication.java
+++ client-service/src/main/java/com/clientservice/app/ClientServiceApplication.java
@@ -9,13 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
 import java.net.URI;
 import java.util.Date;
 import java.util.UUID;
-
+@EnableEurekaClient
 @SpringBootApplication
 public class ClientServiceApplication implements CommandLineRunner {
 	private static final Logger log = LoggerFactory.getLogger(ClientServiceApplication.class);
